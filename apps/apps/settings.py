41909e0a5c54e57fc60753407dc982f7f919dad5
zhanyunpeng1996 <136688059@qq.com>
2019-08-18 14:27:38 +0800

    enable cors

--- apps/apps/settings.py
+++ apps/apps/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'blog.apps.BlogConfig',
     'shell_plus',
-    'django_extensions'
+    'django_extensions',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'apps.urls'
 
