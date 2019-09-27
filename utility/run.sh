aa890a5170b145275a2eb8247919745dfdca433b
zhanyunpeng1996 <136688059@qq.com>
2019-09-28 02:34:47 +0800

    update:更新wsgi配置

--- utility/run.sh
+++ utility/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python manage.py collectstatic --noinput
-gunicorn config.wsgi -w 8 -t 60 --bind 0.0.0.0:5000 --chdir $PWD
+gunicorn apps.wsgi -w 4 -t 60 --bind 0.0.0.0:5000 --chdir $PWD
