0b456dd3c39f8a12914f7e603f21761e3f2bada7
zhanyunpeng1996 <136688059@qq.com>
2019-11-05 20:30:07 +0800

    update

--- utility/run.sh
+++ utility/run.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
-mv utility/base.py /usr/local/lib/python3.8/site-packages/django/db/backends/mysql/base.py
 python manage.py collectstatic --noinput
 gunicorn apps.wsgi -w 4 -t 60 --bind 0.0.0.0:5000 --chdir $PWD
