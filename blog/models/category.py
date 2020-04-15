99e251001bf8c48c3851ab4314aa965b102cd035
yunpeng.zhan <yunpeng.zhan@mail.hypers.com>
2020-04-15 09:13:59 +0800

    update:更新分类名字 唯一约束

--- blog/models/category.py
+++ blog/models/category.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(verbose_name="类别", max_length=50, default="默认")
+    name = models.CharField(verbose_name="类别", max_length=50, unique=True)
 
     class Meta:
         verbose_name = "分类"
